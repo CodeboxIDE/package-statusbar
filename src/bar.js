6a95726173ddfdb3527f066c9ac001072e8a6e25
Samy Pessé <samypesse@gmail.com>
2014-07-21 10:40:22 -0700

    Base view for statusbar
    Complete command to toggle statusbar

---
+++ src/bar.js
@@ -0,0 +1,9 @@
+define(function() {
+    var hr = codebox.require("hr/hr");
+
+    var StatusBar = hr.View.extend({
+        className: "component-statusbar"
+    });
+
+    return StatusBar;
+});
--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
 define([
+    "src/bar",
     "less!src/stylesheets/main.less"
-], function() {
+], function(StatusBar) {
     var commands = codebox.require("core/commands");
     var dialogs = codebox.require("utils/dialogs");
+
+    // Create the bar and add it to the body
+    var bar = new StatusBar();
+    bar.appendTo(codebox.app.$el);
 
     commands.register({
         id: "view.statusbar.toggle",
         title: "View: Toggle Status Bar",
+        shortcuts: [
+            "mod+b", "mod+k"
+        ],
         run: function() {
-
+            codebox.app.$el.toggleClass("hide-statusbar");
         }
     });
+
+    // Exports statusbar
+    codebox.statusbar = bar;
 });
